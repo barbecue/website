ee9bae1367242b59faff6d55b3d2972176d45172
Tuna <barbeku@protonmail.com>
2024-10-16 10:37:33 +0300

    feat: added animation to navbar

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,17 +1,31 @@
 "use client";
-import React from "react";
-import { Button } from "@/components/ui/button";
-import { LucideMoon } from "lucide-react";
+import React, { useState } from "react";
 import Link from "next/link";
-import { CommandMenu } from "@/components/CommandMenu";
 import { usePathname } from "next/navigation";
 import ThemeSwitcher from "@/components/ThemeSwitcher";
 import { cn } from "@/lib/utils";
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
 
 export default function Navbar() {
   const pathname = usePathname();
+  const { scrollY } = useScroll();
+  const [full, setFullWidth] = useState<boolean>(false);
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    const previous = scrollY.getPrevious();
+    if (previous && latest > previous && latest >= 40) setFullWidth(true);
+    else if (previous && latest < previous && latest <= 40) setFullWidth(false);
+  });
+
   return (
-    <div className="mx-auto mt-5 flex w-full max-w-6xl items-center justify-between rounded-lg border bg-card px-5 py-2.5 drop-shadow-2xl">
+    <motion.div
+      className={cn(
+        full
+          ? "top-5 max-w-xs rounded-full bg-card/30 backdrop-blur-2xl lg:max-w-sm"
+          : "top-0 max-w-6xl rounded-lg bg-card",
+        "sticky z-50 mx-auto mt-5 flex w-full items-center justify-between border px-5 py-2.5 drop-shadow-2xl transition-all duration-500 ease-in-out",
+      )}
+    >
       <div className="transition duration-300">
         <Link
           className={cn(pathname !== "/" ? "text-muted" : "dark:text-white")}
@@ -31,6 +45,6 @@ export default function Navbar() {
       <div className="flex flex-row gap-2">
         <ThemeSwitcher />
       </div>
-    </div>
+    </motion.div>
   );
 }
