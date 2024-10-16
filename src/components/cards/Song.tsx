e11c924d4d31223d8751c1f071f911e07af8bb36
Tuna <barbeku@protonmail.com>
2024-10-16 10:24:15 +0300

    fix: added error state to recently listened songs

--- src/components/cards/Song.tsx
+++ src/components/cards/Song.tsx
@@ -1,30 +1,21 @@
 import React from "react";
 import Image from "next/image";
+import { Song } from "@/components/sections/RecentlyListenedSongs";
 
-export default function Song({
-  song,
-}: {
-  song: {
-    id: string;
-    name: string;
-    artists: any;
-    image: string;
-    external_urls: {
-      spotify: string;
-    };
-  };
-}) {
+export default function SongCard({ song }: { song: Song }) {
   return (
     <div className="flex h-16 w-full cursor-pointer items-center justify-between rounded-lg border px-5 drop-shadow-xl transition-colors duration-300 hover:bg-secondary/80 dark:bg-primary dark:hover:bg-primary/70">
       <div className="flex w-full flex-row items-center gap-2">
-        <Image
-          draggable={false}
-          src={song.image}
-          alt={song.name}
-          width={120}
-          height={120}
-          className="aspect-square size-10 flex-1 rounded-sm bg-secondary"
-        />
+        {song.image && (
+          <Image
+            draggable={false}
+            src={song.image}
+            alt={song.name}
+            width={120}
+            height={120}
+            className="aspect-square size-10 flex-1 rounded-sm bg-secondary"
+          />
+        )}
         <div className="flex w-full flex-col">
           <span className="line-clamp-1 w-full text-sm">{song.name}</span>
           <span className="line-clamp-1 w-full text-sm text-muted">
--- src/components/sections/RecentlyListenedSongs.tsx
+++ src/components/sections/RecentlyListenedSongs.tsx
@@ -1,7 +1,15 @@
 import React from "react";
-import Song from "@/components/cards/Song";
+import SongCard from "@/components/cards/Song";
 import spotifyWebApi from "spotify-web-api-node";
 const spotifyApi = new spotifyWebApi();
+
+export interface Song {
+  image: string | null;
+  artists: SpotifyApi.ArtistObjectSimplified[];
+  name: string;
+  id: string;
+  external_urls: SpotifyApi.ExternalUrlObject;
+}
 
 export default async function RecentlyListenedSongs() {
   const {
@@ -9,34 +17,45 @@ export default async function RecentlyListenedSongs() {
     SPOTIFY_CLIENT_SECRET = "",
     SPOTIFY_REFRESH_TOKEN = "",
   } = process.env;
-  await spotifyApi.setCredentials({
-    clientId: SPOTIFY_CLIENT_ID,
-    clientSecret: SPOTIFY_CLIENT_SECRET,
-  });
-  await spotifyApi.setRefreshToken(SPOTIFY_REFRESH_TOKEN);
-  await spotifyApi.refreshAccessToken().then(function (data) {
+
+  let recentlyPlayed: Song[] = [];
+
+  try {
+    await spotifyApi.setCredentials({
+      clientId: SPOTIFY_CLIENT_ID,
+      clientSecret: SPOTIFY_CLIENT_SECRET,
+    });
+
+    await spotifyApi.setRefreshToken(SPOTIFY_REFRESH_TOKEN);
+
+    const data = await spotifyApi.refreshAccessToken();
     spotifyApi.setAccessToken(data.body.access_token);
-  });
-  const List = await spotifyApi.getMyRecentlyPlayedTracks({ limit: 20 });
-  const recentlyPlayed = List.body.items
-    .map(({ track }) => ({ ...track }))
-    .map(({ album, name, id }) => ({ ...album, name, id }))
-    .map(({ images, external_urls, artists, name, id }) => ({
-      images,
-      external_urls,
-      artists,
-      name,
-      id,
-    }))
-    .map(({ images, external_urls, artists, name, id }) => ({
-      image: images[1].url,
-      external_urls,
-      artists,
-      name,
-      id,
-    }))
-    .filter((v, i, a) => a.findIndex((v2) => v2.id === v.id) === i)
-    .slice(0, 8);
+
+    const List = await spotifyApi.getMyRecentlyPlayedTracks({ limit: 20 });
+
+    recentlyPlayed = List.body.items
+      .map(({ track }) => ({ ...track }))
+      .map(({ album, name, id }) => ({ ...album, name, id }))
+      .map(({ images, external_urls, artists, name, id }) => ({
+        images,
+        external_urls,
+        artists,
+        name,
+        id,
+      }))
+      .map(({ images, external_urls, artists, name, id }) => ({
+        image: images[1]?.url || null,
+        external_urls,
+        artists,
+        name,
+        id,
+      }))
+      .filter((v, i, a) => a.findIndex((v2) => v2.id === v.id) === i)
+      .slice(0, 8);
+  } catch (error) {
+    console.error("Error fetching recently played songs:", error);
+  }
+
   return (
     <div className="flex w-full flex-col gap-3 rounded-lg border bg-card px-5 py-5 drop-shadow-2xl xl:col-span-2">
       <div className="flex w-full flex-row items-center justify-between">
@@ -45,9 +64,15 @@ export default async function RecentlyListenedSongs() {
         </span>
       </div>
       <div className="grid grid-cols-1 gap-x-3 gap-y-2 lg:grid-cols-2">
-        {recentlyPlayed.map((song, index) => (
-          <Song song={song} key={index} />
-        ))}
+        {recentlyPlayed?.length ? (
+          recentlyPlayed?.map((song, index) => (
+            <SongCard song={song} key={index} />
+          ))
+        ) : (
+          <span className="col-span-1 flex items-center justify-center text-muted-foreground lg:col-span-2">
+            There is nothing to see.
+          </span>
+        )}
       </div>
     </div>
   );
